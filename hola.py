06550d212e177f05699a04b2e146282fc24993bb
wencer <wamedina2@hotmail.com>
2025-11-23 19:35:01 -0500

    Segunda versión: se muestra la longitud del nombre

--- hola.py
+++ hola.py
@@ -2,3 +2,8 @@
 #scrip basico que saluda al usuario
 nombre = input("wencer")
 print(f"¡hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
+# hola.py
+# Versión mejorada
+nombre = input("Wencer")
+print(f"¡Hola, {nombre}! Bienvenido a tu primer proyecto con Git y Python.")
+print(f"Tu nombre tiene {len(nombre)} letras.")
