0ab1f00ce084a7253f09496a82011b313db67c0b
wencer <wamedina2@hotmail.com>
2025-11-23 19:38:30 -0500

    Primer commit: calculadora básica

---
+++ calculadora.py
@@ -0,0 +1,32 @@
+# calculadora.py
+# Script básico para operaciones matemáticas
+numero_1 = float(input("Primer número: "))
+numero_2 = float(input("Segundo número: "))
+operacion = input("Operación (+, -, *, /): ")
+if operacion == '+':
+print("Resultado:", numero_1 + numero_2)
+elif operacion == '-':
+print("Resultado:", numero_1 - numero_2)
+elif operacion == '*':
+print("Resultado:", numero_1 * numero_2)
+elif operacion == '/':
+if b != 0:
+print("Resultado:", numero_1 / numero_2)
+else:
+print("Error: no se puede dividir por cero.")
+# calculadora.py
+# Script básico para operaciones matemáticas
+numero_1 = float(input("Primer número: "))
+numero_2 = float(input("Segundo número: "))
+operacion = input("Operación (+, -, *, /): ")
+if operacion == '+':
+print("Resultado:", numero_1 + numero_2)
+elif operacion == '-':
+print("Resultado:", numero_1 - numero_2)
+elif operacion == '*':
+print("Resultado:", numero_1 * numero_2)
+elif operacion == '/':
+if b != 0:
+print("Resultado:", numero_1 / numero_2)
+else:
+print("Error: no se puede dividir por cero.")
